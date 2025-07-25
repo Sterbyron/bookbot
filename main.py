79d214a543224d202b8bd5bdcdb9f88be3b9b18f
Sterbyron <sterbyron@gmail.com>
2025-07-25 18:01:10 +0300

    sort only alphabetical

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_words
+from stats import get_lowered_char_freq
 def get_book_text(filepath):
     with open(filepath) as f:
         book = f.read()
@@ -5,11 +7,8 @@ def get_book_text(filepath):
 
 def main():
     book = get_book_text("books/frankenstein.txt")
-    print(book)
-
+    print(str(count_words(book)) + " words found in the document")
+    lowered = get_lowered_char_freq(book)
+    print(lowered)
 if __name__ == "__main__":
     main()
-
-from stats import count_words
-
-print(str(count_words(get_book_text("books/frankenstein.txt"))) + " words found in the document")
--- stats.py
+++ stats.py
@@ -1,18 +1,15 @@
 def count_words(text):
     words = text.split()
     return len(words)
-def get_book_text(filepath):
-    with open(filepath) as f:
-        book = f.read()
-        return book
-chars = get_book_text("books/frankenstein.txt")
-lowered = chars.lower()
-freq = {}
-
-for c in lowered:
-    if c in freq:
-        freq[c] += 1
-    else:
-        freq[c] = 1
-
-print(freq)
+    
+def get_lowered_char_freq(book):
+        lowered = book.lower()
+        freq = {}
+        for c in lowered:
+            if c in freq:
+                freq[c] += 1
+            else:
+                freq[c] = 1
+        return freq
+def sort_dict_by_value(d):
+    return[{"char": c, "num": n} for c, n in sorted(d.items(), key=lambda item: item[1], reverse=True)]
