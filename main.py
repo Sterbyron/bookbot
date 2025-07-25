403fe583f828ae82541e39bdb28b17b666d38907
Sterbyron <sterbyron@gmail.com>
2025-07-25 18:01:28 +0300

    imported sort  in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import get_lowered_char_freq
+from stats import sort_dict_by_value
 def get_book_text(filepath):
     with open(filepath) as f:
         book = f.read()
@@ -10,5 +11,9 @@ def main():
     print(str(count_words(book)) + " words found in the document")
     lowered = get_lowered_char_freq(book)
     print(lowered)
+    sorted = sort_dict_by_value(lowered)
+    for item in sorted:
+        if item["char"].isalpha():
+           print(item["char"], item["num"])
 if __name__ == "__main__":
     main()
