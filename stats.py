7411bcd9fc8e646c2164796b4bf7d7db9767721a
Sterbyron <sterbyron@gmail.com>
2025-07-25 17:55:08 +0300

    sorting

--- stats.py
+++ stats.py
@@ -1,3 +1,18 @@
 def count_words(text):
     words = text.split()
     return len(words)
+def get_book_text(filepath):
+    with open(filepath) as f:
+        book = f.read()
+        return book
+chars = get_book_text("books/frankenstein.txt")
+lowered = chars.lower()
+freq = {}
+
+for c in lowered:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+
+print(freq)
